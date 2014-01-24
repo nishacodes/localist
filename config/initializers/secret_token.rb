70065a4223e304d96727b6a8aea22b260c572d8e
nishacodes <nishabatra27@gmail.com>
2014-01-23 23:44:50 -0500

    create rails app, add devise, generate migration for places

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfavplaces::Application.config.secret_token = '4f8881544324bc81df98243260c7e8338df28904f4c23a5a2af8e27c05c5ffd23de8faf7264b5c955504a56b55f1548d62e255c258cfc444ac38d2bdb5112f10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Myfavplaces::Application.config.session_store :cookie_store, key: '_myfavplaces_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Myfavplaces::Application.config.session_store :active_record_store
