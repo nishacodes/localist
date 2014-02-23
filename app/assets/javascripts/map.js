8517cd8d531ac1d1dc5c49bcd3fcb3664246d08d
nishacodes <nishabatra27@gmail.com>
2014-02-22 21:27:13 -0500

    refactor map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,10 @@
 // CALL FUNCTION
 $(document).ready(function(){
   initialize();
-  // autoComplete();
 });
 
 // VARIABLES
-
+// ---------------------
 // Custom styles for map
 var styles = [
   {
@@ -38,34 +37,43 @@ var mapOptions = {
 var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 var markers = [];
 var permanentMarkers = [];
-var infoWindowNew = new google.maps.InfoWindow();
+var infowindow = new google.maps.InfoWindow();
 
 // MAIN FUNCTION
+// ---------------------
 function initialize() {
 
-  //I ADDED THIS STUFF -- IT GETS A REFERENCE TO THE MARKER
-  var input = document.getElementById('pac-input');
+  // MAP SETUP
+  // ---------------------
+  // Incorporate styles
+  map.setOptions({styles: styles}); 
+  addSelected();
+  // autoComplete();
+
+  // AUTOCOMPLETE
+  // ---------------------
+  // A reference to the marker created by the search
+  var input = document.getElementById('input');
   var markerNew = new google.maps.Marker({
     map: map
   });
-  // this pushes the marker to the top left position on the map
+  
+  // Pushes the field to the top left position on the map
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  // this makes the input box into a new autocomplete thing
+  // Makes the input box into an autocomplete field
   var autocomplete = new google.maps.places.Autocomplete(input);
-  // this fixes the autocomplete to the bounds of the map so the top results are in your area
+  // Fixes the autocomplete to the bounds of the map so the top results are in your area
   autocomplete.bindTo('bounds', map);
 
-  // this event listener i took from him. it puts down a marker when you change the place, meaning that you type in something and hit enter
-  // NOTE: I also had to put in a new info window up above just to make it work; you can do whatever you want but you need to create a new info window to display the places stuff
+  // This event listener remove the old marker and infowindow when you change the place searched
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    infoWindowNew.close();
+    infowindow.close();
     markerNew.setVisible(false);
     var place = autocomplete.getPlace();
     if (!place.geometry) {
       return;
     }
-
     // If the place has a geometry, then present it on a map.
     if (place.geometry.viewport) {
       map.fitBounds(place.geometry.viewport);
@@ -92,19 +100,15 @@ function initialize() {
       ].join(' ');
     }
 
-    infoWindowNew.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    infoWindowNew.open(map, markerNew);
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, markerNew);
   });
 
-  // THIS IS THE END OF THE STUFF I ADDED.
 
-  // Incorporate styles
-  map.setOptions({styles: styles}); 
-  addSelected();
-
+  // LIST FILTER CONTROLS
+  // ---------------------
   // Toggle 'all' to control other selectors
   $("#filters li").first().on('click',function(){
-    console.log(this);
     $(this).toggleClass("");
     if($(this).hasClass("selected")){
       $(this).nextAll().removeClass("selected");
@@ -180,90 +184,6 @@ function initialize() {
         })(marker, i));
     }
   }
-  function autoComplete(){
-    var input = /** @type {HTMLInputElement} */(
-    document.getElementById('pac-input'));
-    
-    var types = document.getElementById('type-selector');
-      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-      map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
-
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.bindTo('bounds', map);
-
-    var new_marker = new google.maps.Marker({
-      map: map
-    });
-
-    google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      infowindow.close();
-      new_marker.setVisible(false);
-      var place = autocomplete.getPlace();
-      if (!place.geometry) {
-        return;
-      }
-
-      // If the place has a geometry, then present it on a map.
-      if (place.geometry.viewport) {
-        map.fitBounds(place.geometry.viewport);
-      } else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(17);  // Why 17? Because it looks good.
-      }
-      new_marker.setIcon(/** @type {google.maps.Icon} */({
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(35, 35)
-      }));
-      new_marker.setPosition(place.geometry.location);
-      new_marker.setVisible(true);
-
-      var address = '';
-      if (place.address_components) {
-        address = [
-          (place.address_components[0] && place.address_components[0].short_name || ''),
-          (place.address_components[1] && place.address_components[1].short_name || ''),
-          (place.address_components[2] && place.address_components[2].short_name || '')
-        ].join(' ');
-      }
-
-      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-      infowindow.open(map, new_marker);
-
-      console.log(place.photos);
-      // POPULATE HIDDEN FORM FIELDS
-      $("#placeid").val(place.id);
-      $("#name").val(place.name);
-      $("#latitude").val(place.geometry.location.d);
-      $("#longitude").val(place.geometry.location.e);
-      $("#phone").val(place.formatted_phone_number);
-      $("#address").val(place.formatted_address);
-      // $("#city").val(place.address_components[5].short_name); // inaccurate
-      // $("#state").val(place.address_components[7].short_name); // inaccurate
-      // $("#postal").val(place.address_components[6].short_name); // inaccurate
-      // $("#country").val(place.address_components[6].short_name); // inaccurate
-      $("#website").val(place.website);
-
-    });
-
-    // Sets a listener on a radio button to change the filter type on Places
-    // Autocomplete.
-    function setupClickListener(id, types) {
-      var radioButton = document.getElementById(id);
-      google.maps.event.addDomListener(radioButton, 'click', function() {
-        autocomplete.setTypes(types);
-      });
-    }
-
-    setupClickListener('changetype-all', []);
-    setupClickListener('changetype-establishment', ['establishment']);
-    setupClickListener('changetype-geocode', ['geocode']);
-
-
-  }
-
 }
 
 // Hover effects
