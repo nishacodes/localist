5736ff7b394b8344abf230a1bcb42f4aba597991
nishacodes <nishabatra27@gmail.com>
2014-03-05 23:08:39 -0500

    little things

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,9 +11,9 @@ class ListsController < ApplicationController
       @blacklist = current_user.blacklists.map {|b| b.place}
       @lists = List.where(user_id: current_user.id).reverse
       if @lists.length == 0
-        List.create(name: "Bar scene", user_id: current_user.id)
         List.create(name: "Casual bites", user_id: current_user.id)
         List.create(name: "Brunch favs", user_id: current_user.id)
+        List.create(name: "Bar scene", user_id: current_user.id)
         @lists = List.where(user_id: current_user.id).reverse
       end
       @places = @lists.map {|list| list.places}
