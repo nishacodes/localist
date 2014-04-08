cb306a06258ef08506dc0e9697c408c77b118418
nishacodes <nishabatra27@gmail.com>
2014-04-08 00:38:12 -0400

    show registered friends on discovecover page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
 
   def discover
     @friends = current_user.get_friends
+    uids = @friends.collect {|f| f['id']} # collects all the ids of friends
+    @registered_friends = User.where('uid IN (?)', uids) # collects all users that are friends
   end
 
 end
